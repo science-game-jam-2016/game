9f7db49ebcca2586d0a0647e43bf365a7e329c6e
nacgarg <nachiketa.gargi@gmail.com>
2016-04-23 11:45:24 -0700

    plant framework

---
+++ js/plant.js
@@ -0,0 +1,3 @@
+var Plant = function () {
+	
+};
---
+++ js/plants/potato.js
@@ -0,0 +1,5 @@
+var Potato = function() {
+	Plant.apply(this, arguments)
+	this.name = "Potato"
+	this.waterPerDay = 10 // TODO refine this value
+}

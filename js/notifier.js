e7ee4eaa0aa9f516a2e16cf815954504245e4f06
enderquestral <hannahlaila@aol.com>
2016-04-24 10:22:25 -0700

    Update notifier.js

--- js/notifier.js
+++ js/notifier.js
@@ -21,11 +21,11 @@ var notifier = {
     }
 }
 
-function show_popup() {
-  var p = window.createPopup()
-  var pbody = p.document.body
-  pbody.style.backgroundColor = "lime"
-  pbody.style.border = "solid black 1px"
-  pbody.innerHTML = "This is a pop-up! Click outside to close."
-  p.show(150,150,200,50,document.body)
-}
+//function show_popup() {
+ // var p = window.createPopup()
+ // var pbody = p.document.body
+ // pbody.style.backgroundColor = "lime"
+ // pbody.style.border = "solid black 1px"
+ // pbody.innerHTML = "This is a pop-up! Click outside to close."
+ // p.show(150,150,200,50,document.body)
+// }

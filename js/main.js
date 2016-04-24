490d5a5919f6dd4da792af51dd0e36ad3ede72d9
enderquestral <hannahlaila@aol.com>
2016-04-24 10:20:08 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 g = new Game()
 
 g.initGrid()
-g.show_popup()
+
 g.plantGrid.setPlantAtLoc(0, 0, new Corn())
 g.plantGrid.setPlantAtLoc(1, 0, new Corn())
 g.plantGrid.setPlantAtLoc(2, 0, new Corn())
+g.show_popup()
 
 document.getElementById('next').onclick = function() {
 	g.nextDay();

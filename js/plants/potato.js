08ab71d96061db11e7aa7244d65989f9fdff3a51
jananisekar14 <jananisekar14@gmail.com>
2016-04-24 12:06:27 -0700

    Update potato.js

--- js/plants/potato.js
+++ js/plants/potato.js
@@ -6,7 +6,7 @@ var Potato = function() {
 	this.dayGrowth = 4
 	this.profitPerDay = 30
 	this.price = 10
-	this.lifetime = 5
+	this.lifetime = this.dayGrowth + 5
 }
 
 Potato.prototype = Object.create(Plant.prototype);
